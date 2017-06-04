bacc65673f76688bce9e8b3d83e9e3f5451377d0
dlukas1 <dmitry.lukas@gmail.com>
2017-06-04 11:37:56 +0300

    first commit

---
+++ Lukas.php
@@ -0,0 +1,21 @@
+<!--Loo tavaline tekstifail (näiteks "salajased_paroolid.txt") ja piira ligipääsu sellele. Faili sisu nägemiseks on vaja teada parooli.-->
+<i>//Parool on "realpass"</i><br><br>
+<form method="post"> 
+	Password: <input type="Password" name="pwd"><br><br>
+	<p><input type="submit"></p>
+</form>
+
+<?php
+	if (isset($_POST['pwd']) && !empty($_POST['pwd'])) {
+		$upass = htmlspecialchars($_POST['pwd']);
+		$realpass = "realpass";
+			if ($upass == $realpass) {
+				echo "salajased_paroolid.txt sisu on: <br/>";
+			include 'salajased_paroolid.txt';
+			//include 'LukasUus.php';
+
+			} else {
+				echo "Vale parool";
+			}
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Loo tavaline tekstifail (näiteks "salajased_paroolid.txt") ja piira ligipääsu sellele. Faili sisu nägemiseks on vaja teada parooli.
+
+Parool on "realpass"
